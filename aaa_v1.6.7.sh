a3ac400ef6724915ff14a203da14c6a0fd3d1912
Tue Sparholt Jørgensen <54983329+tuspjo@users.noreply.github.com>
2022-05-19 14:10:24 +0200

    Update aaa_v1.6.7.sh

--- aaa_v1.6.7.sh
+++ aaa_v1.6.7.sh
@@ -56,7 +56,7 @@ zcat nanopore/*gz|gzip > allnp.fq.gz
 flye -t $THREADS -i 5 -o flye --nano-raw allnp.fq.gz
 
 python ../npgm-contigger/contigger/contigger.py --infile flye/assembly_graph.gfa  --output npgm-contigged.fa 2>nbgm-contigger.err
-flye --polish-target npgm-contigged.fa -o polish --nano-raw allnp.fq.gz
+flye -t $THREADS --polish-target npgm-contigged.fa -o polish --nano-raw allnp.fq.gz
 cat polish/polished_1.fasta|awk '/^>/ { if(NR>1) print "";  printf("%s\n",$0); next; } { printf("%s",$0);}  END {printf("\n");}' > polish.singleline.fa #from user ljq on https://www.biostars.org/p/9262/
 cat polish.singleline.fa |sed '/^$/d'|sed 'N;s/\n/ /'|cat -n - |sed 's/^     //' |sed 's/\t>/ /'|sed 's/^/>contig_/'|sed 's/ /\n/2' > oneline
 cat oneline|grep -v \> |awk '{ print length }' |sed 's/^/length /'|sed 's/$/ nt/'> seqlen
